4155c6f40f2bdabc26918e1f0ae280ec03453121
[peeryo] <peeryo@sfusd.edu>
2025-11-11 18:24:29 -0800

    M5L6 completed

--- lessons/ship.gd
+++ lessons/ship.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+var gem_count := 0
 var max_speed := 1200.0
 var velocity := Vector2(0, 0)
 var steering_factor := 3.0
@@ -31,5 +31,12 @@ func set_health(new_health: int) ->void:
 	get_node("Control/HealthBar").value = health
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	set_health(health + 10)
+	if area_that_entered.is_in_group("gem"):
+		set_gem_count(gem_count +1)
+	elif area_that_entered.is_in_group("healing_item"): 
+		set_health(health + 10)
+	
+func set_gem_count(new_gem_count: int) -> void:
+	gem_count = new_gem_count
+	get_node("Control/GemCount").text = "x" + str(gem_count)
 	
